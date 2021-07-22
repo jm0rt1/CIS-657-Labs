5771777dd520d272d8208e92c13da738465776ce
Jamie Mortensen <16mortensenj@gmail.com>
2021-07-21 20:56:14 -0400

    changed A's prio to 40

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -4,7 +4,7 @@ void sndch(char);
 /* main - just say hello, then exit */ 
 void main(void)
 {
-	resume(create(sndch, 1024, 20, "send A", 1, 'A'));
+	resume(create(sndch, 1024, 40, "send A", 1, 'A'));
 	resume(create(sndch, 1024, 20, "send B", 1, 'B'));
 }
 
