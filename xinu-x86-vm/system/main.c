f4b7f9090c5e70e25e1549bccd3ae8cad3511779
Jamie Mortensen <16mortensenj@gmail.com>
2021-07-21 20:08:21 -0400

    changed main for sec. c

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -1,23 +1,7 @@
-/*  main.c  - main */
-
+/* hello.c - main*/
 #include <xinu.h>
-#include <stdio.h>
-
-int main(int argc, char **argv)
+/* main - just say hello, then exit */ 
+void main(void)
 {
-	uint32 retval;
-
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	recvclr();
-	while (TRUE) {
-		retval = receive();
-		kprintf("\n\n\rMain process recreating shell\n\n\r");
-		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
-	}
-	while (1);
-
-	return OK;
+	printf("Hello, world!\n");
 }
