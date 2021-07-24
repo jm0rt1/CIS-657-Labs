7f352dfe7b57c0b7fff679daf0e2fa0efb40c99f
Jamie Mortensen <16mortensenj@gmail.com>
2021-07-24 13:54:30 -0400

    changes for part1-1

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -2,29 +2,7 @@
 
 #include <xinu.h>
 #include <stdio.h>
-#include <queue.h>
 
-int main(int argc, char **argv)
-{
-	uint32 retval;
-
-	resume(create(p1, 8192, 50, "1", 1, CONSOLE));
-	resume(create(p2, 8192, 40, "2", 1, CONSOLE));
-	resume(create(p3, 8192, 30, "3", 1, CONSOLE));
-	resume(create(p4, 8192, 20, "4", 1, CONSOLE));
-	printf("started");
-	for (int i=0; i < NQENT; i++)
-	{
-		int32 key = queuetab[NQENT].qkey;
-		qid16 prev = queuetab[NQENT].qprev;
-		qid16 next = queuetab[NQENT].qnext;
-		printf("Entry %d: key = %d, next = %d, prev = %d", i,key,next,prev);
-	}
-	printf("finished");
-	return OK;
-
-
-}
 
 void p1(void)
 {
@@ -55,4 +33,31 @@ void p4(void)
 	{
 
 	}
+}
+void print_q_tab(void){
+	printf("started");
+	int i=0;
+	for (i; i < NQENT; i++)
+	{
+		int32 key = queuetab[NQENT].qkey;
+		qid16 prev = queuetab[NQENT].qprev;
+		qid16 next = queuetab[NQENT].qnext;
+		printf("Entry %d: key = %d, next = %d, prev = %d", i,key,next,prev);
+	}
+	printf("finished");
+}
+
+int main(int argc, char **argv)
+{
+
+	resume(create(p1, 8192, 40, "1", 1, CONSOLE));
+	resume(create(p2, 8192, 30, "2", 1, CONSOLE));
+	resume(create(p3, 8192, 20, "3", 1, CONSOLE));
+	resume(create(p4, 8192, 20, "4", 1, CONSOLE));
+	resume(create(print_q_tab, 8192, 50, "print", 1, CONSOLE));
+
+
+	return OK;
+
+
 }
