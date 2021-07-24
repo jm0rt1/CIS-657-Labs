2f76c3490d05af3e69abb9a4afa3455c06d7cc16
Jamie Mortensen <16mortensenj@gmail.com>
2021-07-24 14:25:50 -0400

    something that works for part 1

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -35,27 +35,34 @@ void p4(void)
 	}
 }
 void print_q_tab(void){
-	printf("started");
+	printf("started\n");
 	int i=0;
-	for (i; i < NQENT; i++)
+	for (i; i < 6; i++)
 	{
-		int32 key = queuetab[NQENT].qkey;
-		qid16 prev = queuetab[NQENT].qprev;
-		qid16 next = queuetab[NQENT].qnext;
-		printf("Entry %d: key = %d, next = %d, prev = %d", i,key,next,prev);
+		int32 key = queuetab[i].qkey;
+		qid16 prev = queuetab[i].qprev;
+		qid16 next = queuetab[i].qnext;
+		printf("Entry %d: key = %d, next = %d, prev = %d\n", i,key,next,prev);
 	}
-	printf("finished");
+	printf("finished queuetab\n");
 }
 
 int main(int argc, char **argv)
 {
+	printf("main running\n");
+	resume(create(p1, 1024, 20, "1", 0));
+	printf("proc1\n");
+	resume(create(p2, 1024, 20, "2", 0));
+	printf("proc2\n");
 
-	resume(create(p1, 8192, 40, "1", 0));
-	resume(create(p2, 8192, 30, "2", 0));
-	resume(create(p3, 8192, 20, "3", 0));
-	resume(create(p4, 8192, 20, "4", 0));
-	resume(create(print_q_tab, 8192, 50, "print", 0));
+	resume(create(p3, 1024, 20, "3", 0));
+	printf("proc3\n");
 
+	resume(create(p4, 1024, 20, "4", 0));
+	printf("proc4\n");
+
+	resume(create(print_q_tab, 1024, 30, "print", 0));
+	printf("main done\n");
 
 	return OK;
 
