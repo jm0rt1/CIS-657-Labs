642bca51553a4f3b4b1775f7222da6f88916e8ae
Jamie Mortensen <16mortensenj@gmail.com>
2021-07-26 17:31:29 -0400

    output works for part 2

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -1,68 +1,48 @@
 /*  main.c  - main */
 
 #include <xinu.h>
-#include <stdio.h>
 
 
-void p1(void)
+
+void waiter(sid32 semaphore, pid32 spid)
+{
+	int i = 1;
+	for (i; i<=2000; i++)
+	{
+		// print the count
+		printf("%d ", i);
+		// increment semaphore counter
+
+		wait(semaphore);
+
+	}
+	kill(spid);
+
+}
+
+void signaler(sid32 semaphore)
 {
 	while(1)
 	{
 
-	}
-}
 
-void p2(void)
-{
-	while(1)
-	{
+		printf("signaler is running\n");
+
+		signaln(semaphore, 5);
+
 
 	}
-}
-void p3(void)
-{
-	while(1)
-	{
-
-	}
-}
-
-void p4(void)
-{
-	while(1)
-	{
-
-	}
-}
-void print_q_tab(void){
-	printf("started\n");
-	int i=0;
-	for (i; i < 6; i++)
-	{
-		int32 key = queuetab[i].qkey;
-		qid16 prev = queuetab[i].qprev;
-		qid16 next = queuetab[i].qnext;
-		printf("Entry %d: key = %d, next = %d, prev = %d\n", i,key,next,prev);
-	}
-	printf("finished queuetab\n");
 }
 
 int main(int argc, char **argv)
 {
-	printf("main running\n");
-	resume(create(p1, 1024, 20, "1", 0));
-	printf("proc1\n");
-	resume(create(p2, 1024, 20, "2", 0));
-	printf("proc2\n");
+	sid32 semaphore = semcreate(20);
 
-	resume(create(p3, 1024, 20, "3", 0));
-	printf("proc3\n");
 
-	resume(create(p4, 1024, 20, "4", 0));
-	printf("proc4\n");
-
-	resume(create(print_q_tab, 1024, 30, "print", 0));
-	printf("main done\n");
+	pri16 spid = create(signaler, 1024, 20, "signaler", 1, semaphore);
+	pri16 wpid = create(waiter, 1024, 40, "waiter", 2, semaphore, spid);
+	resume(wpid);
+	resume(spid);
 
 	return OK;
 
