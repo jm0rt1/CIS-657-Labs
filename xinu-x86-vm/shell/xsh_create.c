c68b06abf534fc21409c57796cbbd06aadb9000c
Jamie Mortensen <16mortensenj@gmail.com>
2021-08-11 19:10:00 -0400

    changed create function for breakout room

--- xinu-x86-vm/shell/xsh_create.c
+++ xinu-x86-vm/shell/xsh_create.c
@@ -9,27 +9,34 @@
 */
 shellcmd xsh_create(int nargs, char *args[])
 {
-  pri16 priority;
-  char ch;
-  char *chprio;
-  pid32 pid;
-  if (nargs == 1) {
-    priority=INITPRIO;
-  }
-  else if ( nargs == 2 ) {
-    chprio = args[1];
-    //ch = *chprio++;
-    
-    priority = 0;
-    priority = atoi(chprio);
-    //kprintf("prio = %d", priority);
-  }
-  else {
-    kprintf("Too many arguments\n");
-    return 1;
-  }
-  pid = create(runforever, 1024, priority, "Test", 0);
-  //kprintf("pid = %d\n", pid);
-  resume(pid);
-  return 0;
+    pri16 priority;
+    char ch;
+    char *chprio;
+    pid32 pid;
+    if (nargs == 1) {
+        priority=INITPRIO;
+    }
+    else if (nargs <= 4) 
+    {
+        int i = 1;
+        for (i; i<=nargs;i++)
+        {
+            chprio = args[i];
+            priority = 0;
+            priority = atoi(chprio);
+            pid = create(runforever, 1024, priority, "Test", 0);
+        }
+
+        //ch = *chprio++;
+
+        //kprintf("prio = %d", priority);
+    }
+    else {
+            kprintf("Too many arguments\n");
+            return 1;
+    }
+
+    //kprintf("pid = %d\n", pid);
+    resume(pid);
+    return 0;
 }
